a7d92b34f7580ecad88d43c64e25e3abd3271827
UlilAzmi <zulhaj123@gmail.com>
2025-08-12 11:44:01 +0700

    sdt master data training

--- app/Http/Controllers/Master/TrainingClientController.php
+++ app/Http/Controllers/Master/TrainingClientController.php
@@ -20,16 +20,31 @@ class TrainingClientController extends Controller
 
     public function list(Request $request){
         try {
-            $data = DB::table('m_training_client as cl')
-            ->leftJoin('m_training_area as area','area.id','=', 'cl.area_id')
-            ->select("cl.id", "cl.client", "area.area as area_name", "cl.kab_kota", "cl.tgl_gabung", "cl.jml_karyawan", "cl.target_per_tahun") 
-            ->where('cl.is_aktif', 1)
+            // $data = DB::table('m_training_client as cl')
+            // ->leftJoin('m_training_area as area','area.id','=', 'cl.area_id')
+            // ->select("cl.id", "cl.client", "area.area as area_name", "cl.kab_kota", "cl.tgl_gabung", "cl.jml_karyawan", "cl.target_per_tahun") 
+            // ->where('cl.is_aktif', 1)
+            // ->get();
+
+            $data = DB::table('sl_site as site')
+            ->leftJoin('sl_leads as lead', 'site.leads_id', '=', 'lead.id')
+            ->leftJoin('sl_pks as pks', 'site.leads_id', '=', 'pks.leads_id')
+            ->leftJoin('m_branch as branch', 'lead.branch_id', '=', 'branch.id')
+            ->select(
+                'site.id',
+                'site.nama_site as client',
+                'branch.name as area_name',
+                'site.kota as kab_kota',
+                DB::raw("DATE_FORMAT(pks.created_at, '%d-%m-%Y') as tgl_gabung"),
+                'site.training_tahun as target_per_tahun'
+            )
+            ->whereNull('site.deleted_at')
             ->get();
+
             return DataTables::of($data)
                 ->addColumn('aksi', function ($data) {
                     return '<div class="justify-content-center d-flex">
                         <a href="'.route('training-client.view',$data->id).'" class="btn-view btn btn-info waves-effect btn-xs"><i class="mdi mdi-eye"></i>&nbsp;View</a>&nbsp;
-                        <div class="btn-delete btn btn-danger waves-effect btn-xs" data-id="'.$data->id.'"><i class="mdi mdi-trash-can"></i>&nbsp;Delete</div>
                     </div>';
                 })
                 ->rawColumns(['aksi'])
@@ -50,7 +65,24 @@ class TrainingClientController extends Controller
 
     public function view(Request $request,$id){
         try {
-            $data = DB::table('m_training_client')->where('id',$id)->first();
+            // $data = DB::table('sl_site')->where('id',$id)->first();
+            $data = DB::table('sl_site as site')
+            ->leftJoin('sl_leads as lead', 'site.leads_id', '=', 'lead.id')
+            ->leftJoin('sl_pks as pks', 'site.leads_id', '=', 'pks.leads_id')
+            ->leftJoin('m_branch as branch', 'lead.branch_id', '=', 'branch.id')
+            ->leftJoin('m_company as company', 'pks.company_id', '=', 'company.id')
+            ->select(
+                'site.id',
+                'site.nama_site',
+                'branch.name as area',
+                'site.kota',
+                'company.name as bu',
+                DB::raw("DATE_FORMAT(pks.created_at, '%d-%m-%Y') as tgl_gabung"),
+                'site.training_tahun as training_tahun'
+            )
+            ->where('site.id', $id)
+            ->first();
+
             $listArea = DB::table('m_training_area')->where('is_aktif', 1)->orderBy('area', 'ASC')->get();
             $listBu = DB::table('m_training_laman')->orderBy('laman', 'ASC')->get();
 
@@ -66,7 +98,7 @@ class TrainingClientController extends Controller
             DB::beginTransaction();
 
             $validator = Validator::make($request->all(), [
-                'client' => 'required'
+                'target_per_tahun' => 'required'
             ]);
     
             if ($validator->fails()) {
@@ -76,29 +108,33 @@ class TrainingClientController extends Controller
             
                 $msgSave = '';
                 if(!empty($request->id)){
-                    DB::table('m_training_client')->where('id',$request->id)->update([
-                        'client'       => $request->client,
-                        'laman_id'     => $request->laman_id,
-                        'area_id'     => $request->area_id,
-                        'kab_kota'     => $request->kab_kota,
-                        'jml_karyawan'     => $request->jml_karyawan,
-                        'tgl_gabung'     => $request->tgl_gabung,
-                        'target_per_tahun'     => $request->target_per_tahun,
-                        'updated_at'    => $current_date_time
+                    // DB::table('m_training_client')->where('id',$request->id)->update([
+                    //     'client'       => $request->client,
+                    //     'laman_id'     => $request->laman_id,
+                    //     'area_id'     => $request->area_id,
+                    //     'kab_kota'     => $request->kab_kota,
+                    //     'jml_karyawan'     => $request->jml_karyawan,
+                    //     'tgl_gabung'     => $request->tgl_gabung,
+                    //     'target_per_tahun'     => $request->target_per_tahun,
+                    //     'updated_at'    => $current_date_time
+                    // ]);
+
+                    DB::table('sl_site')->where('id',$request->id)->update([
+                        'training_tahun'     => $request->target_per_tahun
                     ]);
                 }else{
-                    DB::table('m_training_client')->insert([
-                        'client'       => $request->client,
-                        'laman_id'     => $request->laman_id,
-                        'area_id'     => $request->area_id,
-                        'kab_kota'     => $request->kab_kota,
-                        'jml_karyawan'     => $request->jml_karyawan,
-                        'tgl_gabung'     => $request->tgl_gabung,
-                        'target_per_tahun'     => $request->target_per_tahun,
-                        'created_at'    => $current_date_time
-                    ]);
+                    // DB::table('m_training_client')->insert([
+                    //     'client'       => $request->client,
+                    //     'laman_id'     => $request->laman_id,
+                    //     'area_id'     => $request->area_id,
+                    //     'kab_kota'     => $request->kab_kota,
+                    //     'jml_karyawan'     => $request->jml_karyawan,
+                    //     'tgl_gabung'     => $request->tgl_gabung,
+                    //     'target_per_tahun'     => $request->target_per_tahun,
+                    //     'created_at'    => $current_date_time
+                    // ]);
                 }
-                $msgSave = 'Training client'.$request->client.' berhasil disimpan.';
+                $msgSave = 'Training client '.$request->client.' berhasil disimpan.';
             }
             DB::commit();
             return redirect()->back()->with('success', $msgSave);
--- resources/views/master/training-client/view.blade.php
+++ resources/views/master/training-client/view.blade.php
@@ -18,40 +18,11 @@
           @csrf
           <input type="hidden" name="id" value="{{$data->id}}">
           
+        
           <div class="row mb-3">
-            <label class="col-sm-2 col-form-label text-sm-end">Bussiness Unit <span class="text-danger">*</span></label>
-            <div class="col-sm-10">
-              <select id="laman_id" name="laman_id" class="form-select @if ($errors->any()) @if($errors->has('laman_id')) is-invalid @else   @endif @endif" data-allow-clear="true" tabindex="-1">
-              <option value="">- Pilih Bussiness Unit -</option>  
-                @foreach($listBu as $value)
-                <option value="{{$value->id}}" @if($data->laman_id == $value->id) selected @endif>{{$value->laman}}</option>
-                @endforeach
-              </select>
-              @if($errors->has('laman_id'))
-                <div class="invalid-feedback">{{$errors->first('laman_id')}}</div>
-              @endif
-            </div>
-          </div>
-
-          <div class="row mb-3">
-            <label class="col-sm-2 col-form-label text-sm-end">Area <span class="text-danger">*</span></label>
-            <div class="col-sm-6">
-              <select id="area_id" name="area_id" class="form-select @if ($errors->any()) @if($errors->has('area_id')) is-invalid @else   @endif @endif" data-allow-clear="true" tabindex="-1">
-              <option value="">- Pilih Area -</option>  
-                  @foreach($listArea as $value)
-                  <option value="{{$value->id}}" @if($data->area_id == $value->id) selected @endif>{{$value->area}}</option>
-                  @endforeach
-                </select>
-                @if($errors->has('area_id'))
-                  <div class="invalid-feedback">{{$errors->first('area_id')}}</div>
-                @endif
-            </div>
-          </div>
-
-          <div class="row mb-3">
-            <label class="col-sm-2 col-form-label text-sm-end">Client <span class="text-danger">*</span></label>
-            <div class="col-sm-10">
-              <input type="text" id="client" name="client" value="{{$data->client}}" class="form-control @if ($errors->any()) @if($errors->has('client')) is-invalid @else   @endif @endif">
+            <label class="col-sm-2 col-form-label text-sm-end">Client </label>
+            <div class="col-sm-5">
+              <input readonly type="text" id="client" name="client" value="{{$data->nama_site}}" class="form-control @if ($errors->any()) @if($errors->has('client')) is-invalid @else   @endif @endif">
               @if($errors->has('client'))
                   <div class="invalid-feedback">{{$errors->first('client')}}</div>
               @endif
@@ -59,35 +30,43 @@
           </div>
 
           <div class="row mb-3">
-            <label class="col-sm-2 col-form-label text-sm-end">Kab / Kota <span class="text-danger">*</span></label>
-            <div class="col-sm-2">
-              <input type="text" id="kab_kota" name="kab_kota" value="{{$data->kab_kota}}" class="form-control @if ($errors->any()) @if($errors->has('kab_kota')) is-invalid @else   @endif @endif">
+            <label class="col-sm-2 col-form-label text-sm-end">Bisnis Unit </label>
+            <div class="col-sm-5">
+              <input readonly type="text" id="area" name="area" value="{{$data->bu}}" class="form-control @if ($errors->any()) @if($errors->has('area')) is-invalid @else   @endif @endif">
+            </div>
+          </div>
+
+          <div class="row mb-3">
+            <label class="col-sm-2 col-form-label text-sm-end">Area </label>
+            <div class="col-sm-5">
+              <input readonly type="text" id="area" name="area" value="{{$data->area}}" class="form-control @if ($errors->any()) @if($errors->has('area')) is-invalid @else   @endif @endif">
+            </div>
+          </div>
+
+          <div class="row mb-3">
+            <label class="col-sm-2 col-form-label text-sm-end">Kab / Kota </label>
+            <div class="col-sm-5">
+              <input readonly type="text" id="kab_kota" name="kab_kota" value="{{$data->kota}}" class="form-control @if ($errors->any()) @if($errors->has('kab_kota')) is-invalid @else   @endif @endif">
               @if($errors->has('kab_kota'))
                   <div class="invalid-feedback">{{$errors->first('kab_kota')}}</div>
-              @endif
-            </div>
-
-            <label class="col-sm-2 col-form-label text-sm-end">Jumlah Karyawan <span class="text-danger">*</span></label>
-            <div class="col-sm-2">
-              <input type="number" id="jml_karyawan" name="jml_karyawan" value="{{$data->jml_karyawan}}" class="form-control @if ($errors->any()) @if($errors->has('jml_karyawan')) is-invalid @else   @endif @endif">
-              @if($errors->has('jml_karyawan'))
-                  <div class="invalid-feedback">{{$errors->first('jml_karyawan')}}</div>
               @endif
             </div>
           </div>
 
           <div class="row mb-3">
-            <label class="col-sm-2 col-form-label text-sm-end">Tanggal Gabung <span class="text-danger">*</span></label>
-            <div class="col-sm-2">
-              <input type="date" id="tgl_gabung" name="tgl_gabung" value="{{$data->tgl_gabung}}" class="form-control @if ($errors->any()) @if($errors->has('tgl_gabung')) is-invalid @else   @endif @endif">
-              @if($errors->has('tgl_gabung'))
-                  <div class="invalid-feedback">{{$errors->first('tgl_gabung')}}</div>
+            <label class="col-sm-2 col-form-label text-sm-end">Tanggal Gabung </label>
+            <div class="col-sm-5">
+              <input readonly type="text" id="kab_kota" name="kab_kota" value="{{$data->tgl_gabung}}" class="form-control @if ($errors->any()) @if($errors->has('kab_kota')) is-invalid @else   @endif @endif">
+              @if($errors->has('kab_kota'))
+                  <div class="invalid-feedback">{{$errors->first('kab_kota')}}</div>
               @endif
             </div>
+          </div>
 
+          <div class="row mb-3">
             <label class="col-sm-2 col-form-label text-sm-end">Target Per Tahun <span class="text-danger">*</span></label>
             <div class="col-sm-2">
-              <input type="number" id="target_per_tahun" name="target_per_tahun" value="{{$data->target_per_tahun}}" class="form-control @if ($errors->any()) @if($errors->has('target_per_tahun')) is-invalid @else   @endif @endif">
+              <input type="number" id="target_per_tahun" name="target_per_tahun" value="{{$data->training_tahun}}" class="form-control @if ($errors->any()) @if($errors->has('target_per_tahun')) is-invalid @else   @endif @endif">
               @if($errors->has('target_per_tahun'))
                   <div class="invalid-feedback">{{$errors->first('target_per_tahun')}}</div>
               @endif
@@ -115,7 +94,7 @@
           <div class="card-body">
             <div class="col-12 text-center">
               <button id="btn-update" class="btn btn-primary w-100 waves-effect waves-light">
-                <span class="me-1">Update Client</span>
+                <span class="me-1">Update Target Training</span>
                 <i class="mdi mdi-content-save scaleX-n1-rtl"></i>
               </button>
             </div>
